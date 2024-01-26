c99529b35093230f1c126d9490753a3861459ae9
jihyeon2434 <ubis970420@gmail.com>
2024-01-26 17:10:55 +0900

    MVC, count 변수의 값 초기화 및 증가

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -6,10 +6,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	
+	private int count = 0;
+	
+	public UsrHomeController() {
+		count = 0;
+	}
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하쇼♡ 지현입니다♡";
 		
 	}
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가";
+		
+	}
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public int showMain3() {
+		int a = 1;
+		int b = 2;
+		return a+b;
+		
+	}
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+		return count++;
+	}	
+	
 }
