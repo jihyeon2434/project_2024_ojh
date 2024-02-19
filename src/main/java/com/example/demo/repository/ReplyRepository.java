3228e2dfc68af20634944cd6eb3b637840f78631
jihyeon2434 <ubis970420@gmail.com>
2024-02-19 11:41:57 +0900

    댓글DB 시나리오 추가+ 테스트데이터를 article detail 에 표시, 관련 구조 추가

--- src/main/java/com/example/demo/repository/ReplyRepository.java
+++ src/main/java/com/example/demo/repository/ReplyRepository.java
@@ -2,26 +2,23 @@ package com.example.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
-import com.example.demo.vo.Article;
+import com.example.demo.vo.Reply;
 
 @Mapper
 public interface ReplyRepository {
 
-	@Insert("""
-			INSERT INTO
-			article SET
-			regDate = NOW(),
-			updateDate = NOW(),
-			memberId = #{memberId},
-			title = #{title}, `body` = #{body}
+	@Select("""
+				SELECT R.*, M.nickname AS extra__writer
+				FROM reply AS R
+				INNER JOIN `member` AS M
+				ON R.memberId = M.id
+				WHERE relTypeCode = #{relTypeCode}
+				AND relId = #{relId}
+				ORDER BY R.id ASC;
 			""")
-	public void writeReply(int memberId, String title, String body);
+	List<Reply> getForPrintReplies(int loginedMemberId, String relTypeCode, int relId);
 
-	
 }
--- src/main/java/com/example/demo/service/ReplyService.java
+++ src/main/java/com/example/demo/service/ReplyService.java
@@ -5,31 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.repository.ArticleRepository;
-import com.example.demo.util.Ut;
-import com.example.demo.vo.Article;
-import com.example.demo.vo.ResultData;
+import com.example.demo.repository.ReplyRepository;
+import com.example.demo.vo.Reply;
 
 @Service
 public class ReplyService {
 
 	@Autowired
-	private ArticleRepository articleRepository;
+	private ReplyRepository replyRepository;
 
-	public ReplyService(ArticleRepository articleRepository) {
-		this.articleRepository = articleRepository;
+	public ReplyService(ReplyRepository replyRepository) {
+		this.replyRepository = replyRepository;
 	}
 
-	
-
-//	public static ResultData<Integer> writeContent(int loginedMemberId, String content, String relTypeCode, String relId) {
-//		replyRepository.writeReply(int loginedMemberId, String content, relTypeCode,relId);
-
-//		int id = replyRepository.getLastInsertId();
-
-//		return ResultData.from("S-1", Ut.f("%d번 글이 생성되었습니다", id), "id", id);
-//	}
-//		return null;
-//	}
+	public List<Reply> getForPrintReplies(int loginedMemberId, String relTypeCode, int relId) {
+		return replyRepository.getForPrintReplies(loginedMemberId, relTypeCode, relId);
+	}
 
 }
