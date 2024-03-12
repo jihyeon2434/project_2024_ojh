21d3db1042f8f4acf1609baf8781a6334a12bb98
jihyeon2434 <ubis970420@gmail.com>
2024-03-12 23:33:58 +0900

    네이버 '이미지컨설팅' 검색목록 상세데이터의 이미지uri 크롤링(성공)

--- src/main/java/com/example/demo/controller/WebCrawler16.java
+++ src/main/java/com/example/demo/controller/WebCrawler16.java
@@ -1,16 +1,19 @@
 package com.example.demo.controller;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WebCrawler16 {
+
     private WebDriver driver;
     private String url;
 
@@ -28,86 +31,116 @@ public class WebCrawler16 {
         driver.get(url);
 
         try {
-            Thread.sleep(5000); // 페이지가 로드될 때까지 충분한 시간 대기
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // 네이버 지도 검색창에 원하는 검색어 입력 후 엔터
         WebElement inputSearch = driver.findElement(By.className("input_search"));
-        inputSearch.sendKeys(location + Keys.ENTER);
+        String inputKey = " 이미지컨설팅";
+        inputSearch.sendKeys(location + inputKey);
+        inputSearch.sendKeys(Keys.ENTER);
 
         try {
-            Thread.sleep(3000); // 검색 결과가 로드될 때까지 충분한 시간 대기
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // iframe 안의 데이터에 접근하기 위해 iframe으로 전환
         driver.switchTo().frame("searchIframe");
 
-        // 검색 결과에서 각 업체의 정보를 가져옴
+        WebElement scrollBox = driver.findElement(By.id("_pcmap_list_scroll_container"));
+
+        Actions builder = new Actions(driver);
+
+        for (int i = 0; i < 6; i++) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrollBox);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         List<WebElement> elements = driver.findElements(By.className("YwYLL"));
-        for (WebElement element : elements) {
-            // 업체 이름 클릭하여 상세 정보 페이지로 이동
-            element.click();
+
+        for (WebElement e : elements) {
+            e.click();
+            String key = e.getText();
 
             try {
-                Thread.sleep(2000); // 페이지가 로드될 때까지 충분한 시간 대기
+                Thread.sleep(2000);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
 
-            // 상세 정보 페이지에서 필요한 정보 추출
-            String name = driver.findElement(By.className("place_name")).getText();
+            driver.switchTo().parentFrame();
+            driver.switchTo().frame(driver.findElement(By.id("entryIframe")));
+
             String address = driver.findElement(By.className("LDgIH")).getText();
-            String phoneNumber = null;
+
+            String phoneNumber;
             try {
                 phoneNumber = driver.findElement(By.className("xlx7Q")).getText();
-            } catch (Exception ignored) {
+            } catch (Exception ex) {
+                phoneNumber = null;
             }
-            String businessHours = null;
+            String businessHours;
             try {
-                WebElement businessHoursElement = driver.findElement(By.className("xuHjH"));
-                businessHours = businessHoursElement.getText();
-            } catch (Exception ignored) {
+                WebElement button = driver.findElement(By.className("RMgN0"));
+                button.click();
+                List<WebElement> dayElements = driver.findElements(By.xpath("//span[@class='i8cJw']"));
+                List<WebElement> timeElements = driver.findElements(By.xpath("//div[@class='H3ua4']"));
+                StringBuilder businessHoursBuilder = new StringBuilder();
+                for (int j = 0; j < dayElements.size(); j++) {
+                    String day = dayElements.get(j).getText();
+                    String time = timeElements.get(j).getText();
+                    String temp = day + " " + time + "; ";
+                    businessHoursBuilder.append(temp);
+                }
+                businessHours = businessHoursBuilder.toString();
+            } catch (Exception ex) {
+                businessHours = null;
             }
-            String menuInfo = null;
+            String menuInfo;
             try {
-                WebElement menuInfoElement = driver.findElement(By.className("_3ZA0S"));
-                menuInfo = menuInfoElement.getText();
-            } catch (Exception ignored) {
+                List<WebElement> menuEles = driver.findElements(By.className("y0EHb"));
+                List<WebElement> priceEles = driver.findElements(By.className("CLSES"));
+                StringBuilder menuInfoBuilder = new StringBuilder();
+                for (int i = 0; i < menuEles.size(); i++) {
+                    String temp = menuEles.get(i).getText() + ":" + priceEles.get(i).getText() + ";";
+                    menuInfoBuilder.append(temp);
+                }
+                menuInfo = menuInfoBuilder.toString();
+            } catch (Exception ex) {
+                menuInfo = null;
             }
-            String facilities = null;
-            try {
-                WebElement facilitiesElement = driver.findElement(By.className("xPvPE"));
-                facilities = facilitiesElement.getText();
-            } catch (Exception ignored) {
-            }
+
+            // Get image URLs
             List<String> imageUrls = new ArrayList<>();
             try {
                 List<WebElement> imageElements = driver.findElements(By.xpath("//div[@class='K0PDV _div']"));
-                for (WebElement imageElement : imageElements) {
-                    String styleAttribute = imageElement.getAttribute("style");
+                for (WebElement element : imageElements) {
+                    String styleAttribute = element.getAttribute("style");
                     String url = styleAttribute.split("url\\(")[1].split("\\)")[0].replaceAll("'", "").replaceAll("\"", "");
                     imageUrls.add(url);
                 }
-            } catch (Exception ignored) {
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
 
-            // Output data
-            System.out.println("Name: " + name);
+            // Output data including image URLs
+            System.out.println("Name: " + key);
             System.out.println("Address: " + address);
             System.out.println("Phone Number: " + phoneNumber);
             System.out.println("Business Hours: " + businessHours);
             System.out.println("Menu Info: " + menuInfo);
-            System.out.println("Facilities: " + facilities);
             for (String imageUrl : imageUrls) {
                 System.out.println("Image URL: " + imageUrl);
             }
 
-            // 다시 검색 결과 페이지로 돌아가기 위해 뒤로 가기
-            driver.navigate().back();
+            driver.switchTo().parentFrame();
+            driver.switchTo().frame("searchIframe");
         }
 
         driver.quit();
